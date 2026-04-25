8710087898d6103519182afe743eb44dd82717e8
Payton McIntosh <pmcintosh@df12.net>
2026-04-25 14:20:36 +0100

    Add repo skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Skyjoust
+
+This is a generated project using [Copier](https://copier.readthedocs.io/).
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//! `Skyjoust` application entry point.
+
+// TODO: Remove this stub and implement actual application functionality.
+/// Application entry point.
+#[allow(clippy::print_stdout, reason = "CLI output is the intended behaviour")]
+fn main() {
+    println!("Hello from Skyjoust!");
+}
